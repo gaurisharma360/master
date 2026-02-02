65db89cf00e547233faa91ab878b00f553b6d134
joaquinvanschoren <joaquin.vanschoren@gmail.com>
2026-02-02 13:43:55 +0100

    jupyter lab update

--- notebooks/preamble.py
+++ notebooks/preamble.py
@@ -1,4 +1,4 @@
-from IPython.display import set_matplotlib_formats, display, HTML
+from IPython.display import display, HTML
 from IPython.core.interactiveshell import InteractiveShell
 import pandas as pd
 import numpy as np
@@ -9,7 +9,6 @@ import os
 from cycler import cycler
 from pprint import pprint
 
-set_matplotlib_formats('pdf', 'png')
 plt.rcParams['image.cmap'] = "viridis"
 plt.rcParams['image.interpolation'] = "none"
 plt.rcParams['savefig.bbox'] = "tight"
@@ -36,11 +35,6 @@ plt.rcParams["figure.figsize"] = (5, 3)
 plt.rcParams["figure.max_open_warning"] = -1
 plt.rcParams['font.size'] = 8; 
 plt.rcParams['lines.linewidth'] = 0.5
-
-# Presentations
-from notebook.services.config import ConfigManager
-cm = ConfigManager()
-cm.update('livereveal', {'width': '95%', 'height': 786, 'scroll': True, 'theme': 'simple', 'transition': 'fade', 'overflow': 'visible', 'start_slideshow_at': 'selected'})
 
 # Silence warnings
 import warnings
